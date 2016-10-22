e2fc6e91a3795942064d044403edf99dadbcd7a9
Daniel Noskin <dnosk@aol.com>
2016-10-22 16:49:51 -0700

    hello world

---
+++ index.js
@@ -0,0 +1,66 @@
+var firebase = require('firebase');
+firebase.initializeApp({
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+});
+
+var Botkit = require('botkit');
+var controller = Botkit.slackbot();
+controller.configureSlackApp({
+  clientId: process.env.SLACK_CLIENT_ID,
+  clientSecret: process.env.SLACK_CLIENT_SECRET,
+  redirectUri: 'http://localhost:3000/slack/callback',
+  scopes: ['commands']
+});
+
+var request = require('request');
+var express = require('express');
+var app = express()
+app.set('port', process.env.PORT);
+app.listen(app.get('port'));
+
+// TODO: Handle the res.send for success and failure
+app.get('/slack/callback', function(req, res) {
+  var url = 'https://slack.com/api/oauth.access?';
+  url += 'client_id=' + process.env.SLACK_CLIENT_ID;
+  url += '&client_secret=' + process.env.SLACK_CLIENT_SECRET;
+  url += '&code=' + req.query.code;
+
+  request(url, function (error, response, body) {
+    if (error || response.statusCode != 200) {
+      console.log('Request Error: ' + error)
+    } else {
+      var data = JSON.parse(body)
+      if (!data.ok) {
+        console.log('Slack OAuth Error: ' + data.error)
+      } else {
+        var team = {}
+        team[data.team_id] = {
+          access_token: data.access_token,
+          scope: data.scope,
+          user_id: data.user_id,
+          team_id: data.team_id,
+          team_name: data.team_name
+        }
+
+        firebase.database().ref('/slack/').update(team, function(error) {
+          if (error) {
+            console.log('Firebase Error: ' + error)
+          } else {
+            console.log(team[data.team_id].team_name + ' was added to Firebase')
+            res.send('Hello World!');
+          }
+        });
+      }
+    }
+  });
+});
+
+controller.on('slash_command',function(bot,message) {
+  // reply to slash command
+  console.log('slashhh')
+  bot.replyPublic(message,'Everyone can see the results of this slash command');
+});
