874b0f772b49c53ba005cc4e0b2ceaacd26f382b
Daniel Noskin <dnosk@aol.com>
2017-02-27 15:51:32 -0800

    refactor and prep for open source

--- index.js
+++ index.js
@@ -1,23 +1,4 @@
-require ('newrelic');
-
-var firebase = require('firebase');
-firebase.initializeApp({
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
-});
-
-var Botkit = require('botkit');
-var controller = Botkit.slackbot();
-controller.configureSlackApp({
-  clientId: process.env.SLACK_CLIENT_ID,
-  clientSecret: process.env.SLACK_CLIENT_SECRET,
-  redirectUri: 'http://localhost:3000/slack/callback',
-  scopes: ['commands']
-});
-
+// Routing
 var request = require('request');
 var express = require('express');
 var path = require('path');
@@ -28,7 +9,40 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.listen(process.env.PORT || 3000, function() {});
 
+// Firebase
+var firebase = require('firebase');
+firebase.initializeApp({
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+});
+
+// Botkit
+var Botkit = require('botkit');
+var controller = Botkit.slackbot();
+controller.configureSlackApp({
+  clientId: process.env.SLACK_CLIENT_ID,
+  clientSecret: process.env.SLACK_CLIENT_SECRET,
+  redirectUri: 'http://localhost:3000/slack/callback',
+  scopes: ['commands']
+});
+
+// Genius
+var Genius = require('node-genius')
+var genius = new Genius(process.env.GENIUS_ACCESS_TOKEN)
+
+// Mixpanel
+var Mixpanel = require('mixpanel');
+var mixpanel = Mixpanel.init(process.env.MIXPANEL_KEY);
+
+// Routes
 app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/public/index.html')
+})
+
+app.get('*', function(req, res) {
   res.sendFile(__dirname + '/public/index.html')
 })
 
@@ -44,6 +58,7 @@ app.get('/privacy', function (req, res) {
     res.sendFile(__dirname + '/public/privacy.html')
 });
 
+// Slack Authentication
 app.get('/slack/callback', function(req, res) {
   var url = 'https://slack.com/api/oauth.access?';
   url += 'client_id=' + process.env.SLACK_CLIENT_ID;
@@ -83,66 +98,57 @@ app.get('/slack/callback', function(req, res) {
   });
 });
 
+// Slack Firesong
 app.post('/slack/firesong', function(req, res) {
-  var Genius = require('node-genius')
-  var genius = new Genius(process.env.GENIUS_ACCESS_TOKEN)
-  genius.search(req.body.text, function (error, results) {
-    var data = JSON.parse(results)
-    var hits = data.response.hits
-    getSpotifyTrack(hits, function(message, spotify_id) {
-      res.send(message)
+  genius.search('fire', function (error, results) {
+    var dataJSON = JSON.parse(results);
+    var hits = dataJSON.response.hits;
 
-      var Mixpanel = require('mixpanel');
-      var mixpanel = Mixpanel.init(process.env.MIXPANEL_KEY);
-      mixpanel.track('/firesong', {
-        distinct_id: req.body.response_url.substr(33),
-        user_id: req.body.user_id,
-        user_name: req.body.user_name,
-        channel_id: req.body.channel_id,
-        team_id: req.body.team_id,
-        team_domain: req.body.team_domain,
-        text: req.body.text,
-        song: spotify_id
-      });
-    })
+    // Get Random Genius Hit
+    var i = (hits.length < 4 ? hits.length : 3);
+    var random = Math.floor(Math.random() * i);
+
+    // Get Genius song from random song id
+    var geniusId = hits[random].result.id
+    genius.getSong(geniusId, function (error, song) {
+      if (error) {
+        console.error('Get random Genius song error: ', error);
+      } else {
+        var songJSON = JSON.parse(song);
+
+        // Find the Spotify Media
+        var media = songJSON.response.song.media
+        for (index in media) {
+          if (media[index].provider == 'spotify') {
+            var spotifyURL = media[index].url
+            var spotifyId = spotifyURL.substr(37)
+
+            // Send message to Slack
+            var message = {
+              'response_type': 'in_channel',
+              'text': spotifyURL
+            }
+            res.send(message)
+
+            // Record in Mixpanel
+            recordMixpanelEvent(req, spotifyId)
+            break
+          }
+        }
+      }
+    });
   });
 });
 
-function randomGeniusHit(hits, callback) {
-  var i = (hits.length < 4 ? hits.length : 3);
-  var random = Math.floor(Math.random() * i);
-  var track = hits[random].result.title + ' ' + hits[random].result.primary_artist.name
-  getSpotifyTrack(track, hits, function(message, spotify_id) {
-    callback(message, spotify_id)
-  })
-}
-
-function getSpotifyTrack(hits, callback) {
-  var random = Math.floor(Math.random() * hits.length);
-  var track = hits[random].result.title + ' ' + hits[random].result.primary_artist.name
-  var url = 'https://api.spotify.com/v1/search?q=' + encodeURIComponent(track) + '&type=track'
-  request(url, function (error, response, body) {
-    if (error) {
-      getSpotifyTrack(hits, callback)
-    } else {
-      var data = JSON.parse(body)
-      if (data.tracks.items.length == 0) {
-        getSpotifyTrack(hits, callback)
-      } else {
-        var song = {}
-        song[data.tracks.items[0].id] = {
-          artist: data.tracks.items[0].artists[0].name,
-          external_url: data.tracks.items[0].external_urls.spotify,
-          image_url: data.tracks.items[0].album.images[0].url,
-          name: data.tracks.items[0].name,
-          preview_url: data.tracks.items[0].preview_url,
-          url: data.tracks.items[0].uri
-        }
-        callback({
-          'response_type': 'in_channel',
-          'text': data.tracks.items[0].external_urls.spotify
-        }, data.tracks.items[0].uri)
-      }
-    }
-  })
+function recordMixpanelEvent(req, spotify_id) {
+  mixpanel.track('/firesong', {
+    distinct_id: req.body.response_url.substr(33),
+    user_id: req.body.user_id,
+    user_name: req.body.user_name,
+    channel_id: req.body.channel_id,
+    team_id: req.body.team_id,
+    team_domain: req.body.team_domain,
+    text: req.body.text,
+    song: spotify_id
+  });
 }
