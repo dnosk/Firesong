b966ef548e305575abc8dc002348ca93167ebb05
Daniel Noskin <dnosk@aol.com>
2017-02-27 15:57:26 -0800

    fix routing

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ app.get('/', function(req, res) {
   res.sendFile(__dirname + '/public/index.html')
 })
 
-app.get('*', function(req, res) {
+app.get('/?ref=*', function(req, res) {
   res.sendFile(__dirname + '/public/index.html')
 })
 
@@ -100,7 +100,7 @@ app.get('/slack/callback', function(req, res) {
 
 // Slack Firesong
 app.post('/slack/firesong', function(req, res) {
-  genius.search('fire', function (error, results) {
+  genius.search(req.body.text, function (error, results) {
     var dataJSON = JSON.parse(results);
     var hits = dataJSON.response.hits;
 
@@ -122,6 +122,7 @@ app.post('/slack/firesong', function(req, res) {
           if (media[index].provider == 'spotify') {
             var spotifyURL = media[index].url
             var spotifyId = spotifyURL.substr(37)
+            console.log(spotifyURL)
 
             // Send message to Slack
             var message = {
@@ -139,6 +140,46 @@ app.post('/slack/firesong', function(req, res) {
     });
   });
 });
+
+genius.search('fire', function (error, results) {
+    var dataJSON = JSON.parse(results);
+    var hits = dataJSON.response.hits;
+
+    // Get Random Genius Hit
+    var i = (hits.length < 4 ? hits.length : 3);
+    var random = Math.floor(Math.random() * i);
+
+    // Get Genius song from random song id
+    var geniusId = hits[random].result.id
+    genius.getSong(geniusId, function (error, song) {
+      if (error) {
+        console.error('Get random Genius song error: ', error);
+      } else {
+        var songJSON = JSON.parse(song);
+
+        // Find the Spotify Media
+        var media = songJSON.response.song.media
+        for (index in media) {
+          if (media[index].provider == 'spotify') {
+            var spotifyURL = media[index].url
+            var spotifyId = spotifyURL.substr(37)
+            console.log(spotifyURL)
+
+            // Send message to Slack
+            var message = {
+              'response_type': 'in_channel',
+              'text': spotifyURL
+            }
+            res.send(message)
+
+            // Record in Mixpanel
+            recordMixpanelEvent(req, spotifyId)
+            break
+          }
+        }
+      }
+    });
+  });
 
 function recordMixpanelEvent(req, spotify_id) {
   mixpanel.track('/firesong', {
