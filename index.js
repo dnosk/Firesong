2760d44d0c93b2e9aaaa1e540c4715a1b9da5882
Daniel Noskin <dnosk@aol.com>
2017-03-10 16:02:46 -0800

    logging

--- index.js
+++ index.js
@@ -138,6 +138,7 @@ function getRandomGeniusHit(res, hits, attempt) {
         var media = songJSON.response.song.media
         var spotifyMedia = media.map(function(source) { if (source.provider == 'spotify') { return source } })
         if (spotifyMedia) {
+          console.log(spotifyMedia)
           var spotifyURL = spotifyMedia[0].url
           if (spotifyURL.includes('local')) {
             console.log('Local Spotify song id')
